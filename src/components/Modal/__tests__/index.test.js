eaacbccf0d0f88ddf0a7cb2a5b9581145fabfda2
KRD13 <rauchdavis13@gmail.com>
2022-03-12 11:54:05 -0500

    added testing, but can't get it to work correctly

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import PhotoList from './Modal'
+import Modal from '..'
+
+afterEach(cleanup)
+
+const currentPhoto = {
+  name: 'Park bench',
+  category: 'landscape',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+  index: 1
+};
+
+describe('Modal component', () => {
+  it('renders', () => {
+    render(<Modal />);
+  });
+
+  it('renders', () => {
+    const { asFragment } = render(<Modal />)
+    expect(asFragment()).toMatchSnapshot()
+  });
+});
+
+
+describe('Click Event', () => {
+  it('calls onClose handler', () => {
+    const { getByText } = render(<Modal
+      onClose={mockToggleModal}
+      currentPhoto={currentPhoto}
+    />);
+    fireEvent.click(getByText('Close this modal'));
+
+    expect(mockToggleModal).toHaveBeenCalledTimes(1);
+  });
+})  
